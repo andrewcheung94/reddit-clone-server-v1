a4996aa9709c999ece90c8c5bb500cd20afe256d
Andrew Cheung <cheung.andrew94@gmail.com>
2021-05-25 15:42:09 -0700

    rearranged my server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+
 const userRoutes = require("./routes/user");
 const postRoutes = require("./routes/post");
 const loginRoutes = require("./routes/login");
@@ -8,21 +8,26 @@ const loginRoutes = require("./routes/login");
 const { verifyToken } = require("./controllers/authController");
 
 const app = express();
-app.use(express.static(path.join(__dirname, "client/build")));
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-app.use(cors());
+// DataBase Setup
+require("dotenv").config();
 const db = require("./config/database");
 
-app.use(express.json());
+app.use(cors());
 
+// MiddleWare
+app.use(express.json());
 app.use("/users", verifyToken, userRoutes);
 app.use("/posts", verifyToken, postRoutes);
 app.use("/login", loginRoutes);
+
+// Serve ReactJs App
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.use("*", (req, res) => {
     res.sendFile(__dirname + "/client/build/index.html");
 });
-
-app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
+app.listen(port, () => console.log(`server is running on port: ${port}`));
 
 module.exports = app;
