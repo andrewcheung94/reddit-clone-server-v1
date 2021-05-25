f5ca26818248d6aed5a2faa093daf740baf30008
Andrew Cheung <cheung.andrew94@gmail.com>
2021-05-25 16:08:55 -0700

    remove serve --s package

--- config/database.js
+++ config/database.js
@@ -2,11 +2,6 @@ const mongoose = require("mongoose");
 
 const dbURL = process.env.URL;
 
-mongoose
-    .connect(dbURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-    })
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
     .then(() => console.log(`MongoDB successfully connected at ${dbURL}`))
     .catch((err) => console.log(err));
